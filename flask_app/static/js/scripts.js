73e4622c6e02d94e6fa868102a954c4430cf0f73
manortizp <129195092+manortizp@users.noreply.github.com>
2025-05-16 12:54:23 -0400

    Se crea sistema de carpetas de Flask y se agregan archivos a estas

---
+++ flask_app/static/js/scripts.js
@@ -0,0 +1,13 @@
+
+// Botones
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("btn-agregar").addEventListener("click", function() {
+        window.location.href = "agregar-actividad.html"; // 
+    });
+    document.getElementById("btn-listado").addEventListener("click", function() {
+        window.location.href = "listado.html"; 
+    });
+    document.getElementById("btn-estadistica").addEventListener("click", function() {
+        window.location.href = "estadistica.html"; // 
+    });
+});
